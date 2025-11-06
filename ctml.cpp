a1177abcfded3911863f3b82655ca1cd6924d214
Abdullah Hassan <abdullah.hassan@talentadore.com>
2025-11-06 01:35:49 +0100

    update

--- ctml.cpp
+++ ctml.cpp
@@ -1,125 +1,92 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 999999;
-const long long INF = 1e18;
-
 int n, k;
 string s;
+vector<vector<long long>> dp;
+vector<vector<int>> opt;
+vector<vector<long long>> cost;
 
-// Precompute arrays 
-
-vector<int> balance_prefix;
-
-void precompute_balance() {
-    balance_prefix.resize(n + 1);
-    balance_prefix[0] = 0;
+void precompute_costs() {
+    cost.assign(n, vector<long long>(n, 0));
     
-    for (int i = 0; i < n; i++) {
-        if (s[i] == '<')
-            balance_prefix[i + 1] = balance_prefix[i] + 1;
-        else
-            balance_prefix[i + 1] = balance_prefix[i] - 1;
-    }
-}
-
-unordered_map<int, long long> costMemo; //  O(1) lookup
-
-// need to further optimize this
-long long count_valid(int l, int r) {
-    if (l > r)
-        return 0;
-
-    int key = l * MAXN + r;
-    
-    if (costMemo.count(key))
-        return costMemo[key];
-
-    long long count = 0;
-    
-    // n2 -> try to optimize this somehow
-    for (int i = l; i <= r; i++) {
-        int balance = 0;
+    for (int l = 0; l < n; l++) {
+        stack<long long> st;
+        long long dp_val = 0;
+        long long last = 0;
         
-        for (int j = i; j <= r; j++) {
-            balance = balance_prefix[j + 1] - balance_prefix[i];
-            
-            if (balance < 0)
-                break; // optimization early terminztion
-            
-            if (balance == 0)
-                count++;
+        for (int r = l; r < n; r++) {
+            if (s[r] == '<') {
+                st.push(last);
+                last = 0;
+            } else { 
+                if (!st.empty()) {
+                    long long l_val = st.top();
+                    st.pop();
+                    last = l_val + 1;
+                    dp_val += last;
+                } else {
+                    last = 0;
+                }
+            }
+            cost[l][r] = dp_val;
         }
     }
-
-    costMemo[key] = count;
-    return count;
 }
 
-long long dp[MAXN];
-long long new_dp[MAXN];
+long long C(int i, int j) {
+    if (i > j) return 0;
+    return cost[i][j];
+}
 
-// Divide and Conquer
-void compute(int j, int l, int r, int opt_l, int opt_r) {
-    if (l > r)
-        return;
+long long solve() {
+    precompute_costs();
+    
 
-    int mid = (l + r) / 2;
-    long long best = INF;
-    int best_k = -1;
+    dp.assign(k + 1, vector<long long>(n + 1, LLONG_MAX));
+    opt.assign(k + 1, vector<int>(n + 1, 0));
+    
 
-    // all split points in the optimal range kinda n3 here calling count_valid
-    for (int k = opt_l; k <= min(mid - 1, opt_r); k++) {
-        long long curr = dp[k] + count_valid(k + 1, mid);
-        if (curr < best) {
-            best = curr;
-            best_k = k;
+    for (int j = 1; j <= n; j++) {
+        dp[1][j] = C(0, j - 1);
+        opt[1][j] = 0;
+    }
+    
+    //  Knuth's optimization
+
+    for (int i = 2; i <= k; i++) {
+        for (int j = i; j <= n; j++) {
+
+            int lower = (j == i) ? (i - 1) : opt[i][j - 1];
+            int upper = j - 1;
+            
+            long long best_val = LLONG_MAX;
+            int best_split = -1;
+
+            for (int x = lower; x <= upper; x++) {
+                long long val = dp[i - 1][x] + C(x, j - 1);
+                if (val < best_val) {
+                    best_val = val;
+                    best_split = x;
+                }
+            }
+            
+            dp[i][j] = best_val;
+            opt[i][j] = best_split;
         }
     }
-
-    new_dp[mid] = best;
-
-    if (best_k != -1) {
-        compute(j, l, mid - 1, opt_l, best_k);
-        compute(j, mid + 1, r, best_k, opt_r);
-    }
+    
+    return dp[k][n];
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    cin >> n >> k >> s;
-
-    precompute_balance();
-
-    // init DP 
-    for (int i = 0; i < n; i++) {
-        dp[i] = INF;
-        new_dp[i] = INF;
-    }
-
-    // Base case
-    for (int i = 0; i < n; i++) {
-        dp[i] = count_valid(0, i);
-    }
-
-    // DP for j = 2 to k
-    for (int j = 2; j <= k; j++) {
-        for (int i = 0; i < n; i++) {
-            new_dp[i] = INF;
-        }
-
-        // split at positions j-1 or later
-        compute(j, j - 1, n - 1, j - 2, n - 2);
-        for (int i = 0; i < n; i++) {
-            dp[i] = new_dp[i];
-        }
-        
-        if (j % 10 == 0) costMemo.clear();
-    }
-
-    cout << dp[n - 1] << endl;
-
+    
+    cin >> n >> k;
+    cin >> s;
+    
+    cout << solve() << "\n";
+    
     return 0;
 }
